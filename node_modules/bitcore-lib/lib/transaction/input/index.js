b0178dbb548b1f9ba36726397470d948cf967f6b
web-霍文艺 <775249762@qq.com>
2024-09-29 22:44:34 +0800

    Delete node_modules directory

--- node_modules/bitcore-lib/lib/transaction/input/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = require('./input');
-
-module.exports.PublicKey = require('./publickey');
-module.exports.PublicKeyHash = require('./publickeyhash');
-module.exports.MultiSig = require('./multisig');
-module.exports.MultiSigScriptHash = require('./multisigscripthash');
-module.exports.Taproot = require('./taproot');
